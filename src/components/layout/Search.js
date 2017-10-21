f6da506b7d03f4bdc505a251f7f787a53d4b2e19
Joe Kim <josephkim0224@gmail.com>
2017-10-21 09:13:24 -0400

    configure bsic Search component

---
+++ src/components/layout/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+
+  render() {
+    return (
+      <div>
+        This is a Search Component
+      </div>
+    )
+  }
+}
+
+export default Search
