731762a849a19dbdc173d788be26e3b26b9be96c
Joe Kim <josephkim0224@gmail.com>
2017-10-21 09:23:00 -0400

    add couple inputs on Search component

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react'
 
 class Search extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      search: {
+        location: '',
+        query: ''
+      }
+    }
+  }
+
   render() {
     return (
-      <div>
-        This is a Search Component
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <h3>Search Venues</h3>
+            <input type="text" placeholder="Query" /><br />
+            <input type="text" placeholder="Location" /><br />
+            <button>Search</button>
+          </div>
+
+          <div className="col-md-8">
+            
+          </div>  
+        </div>
       </div>
     )
   }
