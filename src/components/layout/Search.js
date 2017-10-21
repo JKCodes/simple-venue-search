e8bc77eb7105b7e5ae74288acf3b4ea9f213a8bc
Joe Kim <josephkim0224@gmail.com>
2017-10-21 09:26:51 -0400

    add dupateSearchFilters onChange event to inputs

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Search extends Component {
 
-  constructor(){
+  constructor() {
     super()
     this.state = {
       search: {
@@ -12,14 +12,18 @@ class Search extends Component {
     }
   }
 
+  updateSearchFilters(event) {
+    console.log('updateSearchFilters: ' + event.target.value)
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-4">
             <h3>Search Venues</h3>
-            <input type="text" placeholder="Query" /><br />
-            <input type="text" placeholder="Location" /><br />
+            <input onChange={this.updateSearchFilters.bind(this)} type="text" placeholder="Query" /><br />
+            <input onChange={this.updateSearchFilters.bind(this)} type="text" placeholder="Location" /><br />
             <button>Search</button>
           </div>
 
