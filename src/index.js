2fcd5d018dd26bc917a7606f712543c274f9ef01
Joe Kim <josephkim0224@gmail.com>
2017-10-21 09:19:40 -0400

    update main index.js to include the Search component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import store from './stores'
 import { Provider } from 'react-redux'
+import { Search } from './components/layout'
 
 const app = (
 	<Provider store={store.configure(null)}>
-		<Intro />
+		<Search />
 	</Provider>
 )
 
